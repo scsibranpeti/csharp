800583c29e5bb118d663f59030be062a257710b6
Péter Scsibrán <95998662+scsibranpeti@users.noreply.github.com>
2022-09-24 09:29:48 +0200

    Add files via upload

---
+++ GC_01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int max = 0;
+            try
+            {
+                while (true)
+                {
+                    NewCalss cl = new NewCalss();
+                    int t = NewCalss.c;
+                    Console.WriteLine(t);
+                    if (t > max)
+                    {
+                        max = t;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            Console.WriteLine("Max:" + max);
+        }
+    }
+}
