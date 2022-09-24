4a9ac724b96b2692eb7b5a7a1a6f42e1aac5f91e
Péter Scsibrán <95998662+scsibranpeti@users.noreply.github.com>
2022-09-24 09:23:15 +0200

    Add files via upload

---
+++ NewCalss.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class NewCalss
+    {
+        public static int c = 0;
+
+
+        public NewCalss()
+        {
+            c++;
+        }
+        ~NewCalss()
+        {
+            c--;
+        }
+
+    }
+}
